1b99c23a51cdd9d37063789642d4afb953d477bd
Michael Läuchli <michael.laeuchli@students.fhnw.ch>
2016-05-31 10:41:46 +0200

    add MouseEntered Event

--- src/Main/StopOverOverview/StopOverOverview.java
+++ src/Main/StopOverOverview/StopOverOverview.java
@@ -1,6 +1,7 @@
 package Main.StopOverOverview;
 
 import Main.StopOver.StopOver;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
@@ -14,23 +15,24 @@ import java.util.LinkedList;
  */
 public class StopOverOverview extends VBox {
 
-    private Button addButton;
+    //private Button addButton;
     private ObservableList<StopOver> stopOverArrayList;
     private static final ObservableList<StopOver> allItems = FXCollections.observableArrayList();
 
     public StopOverOverview(){
         stopOverArrayList = FXCollections.observableArrayList();
-        stopOverArrayList.add(new StopOver());
-        addButton = new Button("hinzufügen");
-        addButton.setOnAction(event -> addItem());
+        allItems.add(new StopOver());
+        addEvent(allItems.get(allItems.size()-1));
+        //addButton = new Button("hinzufügen");
+        //addButton.setOnAction(event -> addItem());
         layoutControls();
     }
 
     private void layoutControls() {
-        for(StopOver stopOver : stopOverArrayList){
+        for(StopOver stopOver : allItems){
            this.getChildren().add(stopOver);
-            addButton.setMaxWidth(Double.MAX_VALUE);
-            getChildren().addAll(addButton);
+            //addButton.setMaxWidth(Double.MAX_VALUE);
+            //getChildren().addAll(addButton);
         }
 
     }
@@ -42,5 +44,19 @@ public class StopOverOverview extends VBox {
 
     public ObservableList<StopOver> getAllItems(){
         return allItems;
+    }
+
+    private void addEvent(StopOver so){
+        so.setOnMouseEntered(event ->
+        {if(true) addItem();
+            addEvent(allItems.get(allItems.size()-1));
+            if(allItems.size()>1){
+                removeEvent(allItems.get(allItems.size()-2));
+            }
+        });
+    }
+    private void removeEvent(StopOver so){
+        so.setOnMouseEntered(event ->
+        {});
     }
 }
