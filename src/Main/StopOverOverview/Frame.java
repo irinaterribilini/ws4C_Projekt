b94382ec1d5c4819a297a465ac7c49eac0ee68a5
irinaterribilini <irina.terribilini@students.fhnw.ch>
2016-06-02 16:24:47 +0200

    addButton

--- src/Main/StopOverOverview/Frame.java
+++ src/Main/StopOverOverview/Frame.java
@@ -1,28 +1,40 @@
 package Main.StopOverOverview;
 
 
+import Main.StopOver.StopOver;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+
 
 /**
  * Created by Michael Läuchli & Irina Terribilini
  */
 public class Frame extends HBox {
 
+    private static final ObservableList<StopOver> allItems = FXCollections.observableArrayList();
     private StopOverOverview stopOverOverview;
-
+    private Button addButton;
 
     public Frame() {
         initializeControls();
         layoutControls();
+        addEventHandlers();
     }
 
     private void initializeControls() {
         stopOverOverview = new StopOverOverview();
+        addButton = new Button("hinzufügen");
     }
 
     private void layoutControls() {
-        getChildren().add(stopOverOverview);
-    }
+        getChildren().addAll(stopOverOverview, addButton);
+        addButton.setMaxWidth(Double.MAX_VALUE);}
+
+    private void addEventHandlers() {
+        addButton.setOnAction(event -> stopOverOverview.addItem());
+}
 
 
 }
--- src/Main/StopOverOverview/StopOverOverview.java
+++ src/Main/StopOverOverview/StopOverOverview.java
@@ -1,38 +1,26 @@
 package Main.StopOverOverview;
 
 import Main.StopOver.StopOver;
-import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 /**
  * Created by Michael Läuchli & Irina Terribilini
  */
 public class StopOverOverview extends VBox {
 
-    //private Button addButton;
     private ObservableList<StopOver> stopOverArrayList;
     private static final ObservableList<StopOver> allItems = FXCollections.observableArrayList();
 
     public StopOverOverview(){
         stopOverArrayList = FXCollections.observableArrayList();
-        allItems.add(new StopOver());
-        addEvent(allItems.get(allItems.size()-1));
-        //addButton = new Button("hinzufügen");
-        //addButton.setOnAction(event -> addItem());
         layoutControls();
     }
 
     private void layoutControls() {
         for(StopOver stopOver : allItems){
            this.getChildren().add(stopOver);
-            //addButton.setMaxWidth(Double.MAX_VALUE);
-            //getChildren().addAll(addButton);
         }
 
     }
@@ -42,21 +30,9 @@ public class StopOverOverview extends VBox {
         this.getChildren().add(allItems.get(allItems.size()-1));
     }
 
+
     public ObservableList<StopOver> getAllItems(){
         return allItems;
     }
 
-    private void addEvent(StopOver so){
-        so.setOnMouseEntered(event ->
-        {if(true) addItem();
-            addEvent(allItems.get(allItems.size()-1));
-            if(allItems.size()>1){
-                removeEvent(allItems.get(allItems.size()-2));
-            }
-        });
-    }
-    private void removeEvent(StopOver so){
-        so.setOnMouseEntered(event ->
-        {});
-    }
 }
