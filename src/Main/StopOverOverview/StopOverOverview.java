bd076941437fd5fb2f22db155d82246ec4cf9adc
Michael Läuchli <michael.laeuchli@students.fhnw.ch>
2016-06-07 10:29:23 +0200

    Bugfix StopoverOverbiew String Constructor

--- src/Main/StopOverOverview/StopOverOverview.java
+++ src/Main/StopOverOverview/StopOverOverview.java
@@ -23,19 +23,21 @@ public class StopOverOverview extends VBox {
         String[][] stopParts = new String[(stops.length)][3];
 
         for(int i = 0; i<stops.length; i++){
-            if(stops[i].charAt(0)==' '){
+            if(stops[i].charAt(0)==(char)' '){
                 stops[i] = stops[i].substring(1);
             }
-            if(stops[i].charAt(stops[i].length()-1)==' '){
-                stops[i] = stops[i].substring(0, stops[i].length()-2);
+            if(stops[i].charAt(stops[i].length()-1)==(char)' '){
+                stops[i] = stops[i].substring(0, stops[i].length()-1);
             }
-            stopParts[i][0] = stops[i].substring(stops[i].length()-5, stops[i].length()-4);
-            stopParts[i][1] = stops[i].substring(stops[i].length()-2, stops[i].length()-1);
+            stopParts[i][0] = stops[i].substring(stops[i].length()-5, stops[i].length()-3);
+            stopParts[i][1] = stops[i].substring(stops[i].length()-2, stops[i].length());
             stopParts[i][2] = stops[i].substring(0, stops[i].length()-7);
         }
         for(int i = 0 ; i < stopParts.length; i++){
             allItems.add(new StopOver(stopParts[i][0],stopParts[i][1],stopParts[i][2]));
         }
+
+        layoutControls();
 
     }
 
